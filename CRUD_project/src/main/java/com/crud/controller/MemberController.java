189509b51c25cfc243a45fc4086b169c5ffc74d0
진영준 <139186194+aladght@users.noreply.github.com>
2023-11-29 23:52:30 +0900

    test commit
    깃허브 사용 숙지를 위한 브랜치 푸쉬

--- CRUD_project/src/main/java/com/crud/controller/MemberController.java
+++ CRUD_project/src/main/java/com/crud/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.crud.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,6 @@ import com.crud.DuplicateIdException;
 import com.crud.form.MemberForm;
 import com.crud.service.MemberService;
 
-import groovyjarjarpicocli.CommandLine.DuplicateNameException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -24,34 +24,54 @@ public class MemberController {
 
 	// 관리자만 보이도록 추후 설정할 것
 	@GetMapping("/signup")
-	public String signup(MemberForm memberForm) {
+	public String signup(@Valid MemberForm memberForm) {
 		return "member/signup_form";
 	}
 
 	// 관리자 회원가입 시큐리티
-	@PostMapping("/signup")
-	public String signup(@Valid MemberForm memberForm, BindingResult bindingResult) {
+	@PostMapping("/")
+	public String signup(@Valid MemberForm memberForm, BindingResult bindingResult, Model model) {
+		
 		if (bindingResult.hasErrors()) {
-			return "member/signup_form";
+			return "redirect:/";
 		}
+		
+		// 비밀번호 일치 확인
 		if (!memberForm.getMbPw1().equals(memberForm.getMbPw2())) {
 			bindingResult.rejectValue("mbPw2", "passwordIncorrect", "비밀번호를 확인해주세요");
-			return "member/signup_form";
+			model.addAttribute("pwMsg", "비밀번호가 일치하지 않습니다.");
+			return "redirect:/";
 		}
+		
+		// 멤버를 DB에 저장하는 로직
 		try {
-			this.memberService.create(memberForm.getMbId(), memberForm.getMbPw1(), memberForm.getMbName(),
-					memberForm.getMbCompany());
-		} catch (DuplicateIdException e) {
+			// 아이디가 있는 없는 지를 판단하는 SERVICE만들기 O
+			// SERVICE 호출하기 O
+			memberService.request(memberForm.getMbId());
+			
+			
+			bindingResult.reject("signupsucess", "요청이 완료되었습니다.");
+			
+			// 중복된 아이디가 없으면 관리자에게 정보를 넘겨주기 X
+			return "redirect:/";
+			
+			// 관리자 페이지로 이동하게 하면 됨. X
+		} catch (Exception e) {
+			
 			bindingResult.reject("signupfail", "이미 등록된 아이디입니다.");
-			return "member/signup_form";
+			return "redirect:/";
 		} 
 
-		return "redirect:/main";
 	}
+	
+	// ------- 관리자 페이지에서 db로 저장을 하는 컨트롤러 및 서비스 로직 구성
+	
+	
 
 	// 가장 처음 시작하는 로그인 페이지
 	@GetMapping("/")
-	public String login() {
+	public String login(Model model) {
+		model.addAttribute("memberForm", new MemberForm());
 		return "member/login_form";
 	}
 
--- CRUD_project/src/main/java/com/crud/service/AnalysisService.java
+++ CRUD_project/src/main/java/com/crud/service/AnalysisService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.crud.DefectTracker;
 import com.crud.entity.Analysis;
 import com.crud.repository.AnalysisRepository;
 
@@ -18,14 +19,33 @@ public class AnalysisService {
 
 	@Autowired
 	private AnalysisRepository analysisRepository;
-
+	
+	 // DefectTracker 객체를 생성합니다.
+    private DefectTracker defectTracker = new DefectTracker();
+	
 	public void imgSave(Analysis analysis) {
 		analysisRepository.save(analysis);
+		
+		defectTracker.checkDefect(analysis.getPredictionJdm());
+		/*
+		  * Description : 이미지 분석 결과를 데이터베이스에 저장하고, 
+		  *               분석 결과가 불량인지 아닌지를 체크합니다.
+		  * Params      : analysis - 이미지 분석 결과가 담긴 Analysis 객체
+		  * Returns     : x
+		*/
 	}
 
 	// Order By해서 리스트 값 리턴하기(이미지)
 	public Page<Analysis> getAllAnalysis(int page) {
 
+		/**
+		 * Description : 제품 분석 결과를 저장한 데이터베이스에서 특정 페이지의 분석 결과 목록을 가져옵니다.
+		 *               가져온 분석 결과 목록은 제품 인덱스(productIdx)를 기준으로 내림차순으로 정렬되며,
+		 *               한 페이지당 10개의 분석 결과를 포함합니다.
+		 * Params      : page - 가져올 분석 결과 목록의 페이지 번호
+		 * Returns     : 해당 페이지의 분석 결과 목록 (Page<Analysis>)
+		 */
+		
 		Pageable pageable = PageRequest.of(page, 10); // 한 페이지당 10개만 보여주겠다는 의미
 		Page<Analysis> PageList = analysisRepository.findAllByOrderByProductIdxDesc(pageable);
 		return PageList;
