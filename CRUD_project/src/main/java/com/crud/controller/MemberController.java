f5f8a82b7119e11abd970863415592f8015b2366
장채린 <139251702+wkdcofls@users.noreply.github.com>
2023-12-02 17:54:35 +0900

    [BND]메인 로그아웃 기능 #18

--- CRUD_project/src/main/java/com/crud/controller/MemberController.java
+++ CRUD_project/src/main/java/com/crud/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.crud.controller;
 
-import groovy.lang.GString;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -13,7 +12,6 @@ import com.crud.DuplicateIdException;
 import com.crud.form.MemberForm;
 import com.crud.service.MemberService;
 
-import groovyjarjarpicocli.CommandLine.DuplicateNameException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -22,95 +20,90 @@ import lombok.RequiredArgsConstructor;
 
 public class MemberController {
 
-	private final MemberService memberService;
+   private final MemberService memberService;
 
-	@GetMapping("myPage")
-	public String myPage(){
-		return "Mypage";
-	}
+   // 관리자만 보이도록 추후 설정할 것
+   @GetMapping("/signup")
+   public String signup(MemberForm memberForm) {
+      return "member/signup_form";
+   }
 
-	// 관리자만 보이도록 추후 설정할 것
-	@GetMapping("/signup")
-	public String signup(MemberForm memberForm) {
-		return "member/signup_form";
-	}
+   @GetMapping("/memberManagement")
+   public String memberManagement(Model model) {
+      model.addAttribute("memberForm", new MemberForm());
+      return "member/memberManagement";
+   }
 
-	@GetMapping("/memberManagement")	
-	public String memberManagement() {
-		return "member/memberManagement";
-	}
-	
-	@PostMapping("/signup")
-	public String signup(@Valid MemberForm memberForm, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			return "member/memberManagement";
-		}
-		if (!memberForm.getMbPw1().equals(memberForm.getMbPw2())) {
-			bindingResult.rejectValue("mbPw2", "passwordIncorrect", "비밀번호를 확인해주세요");
-			return "member/memberManagement";
-		}
-		try {
-			this.memberService.create(memberForm.getMbId(), memberForm.getMbPw1(), memberForm.getMbName(),
-					memberForm.getMbCompany());
-		} catch (DuplicateIdException e) {
-			bindingResult.reject("signupfail", "이미 등록된 아이디입니다.");
-			return "member/memberManagement";
-		} catch (DuplicateNameException e) {
-			bindingResult.reject("signupfail", "이미 등록된 이름입니다.");
-			return "member/memberManagement";
-		}
-		return "redirect:/memberManagement";
-	}
+   // 관리자 회원가입 추가 시큐리티
+   @PostMapping("/memberManagement")
+   public String signup(@Valid MemberForm memberForm, BindingResult bindingResult, Model model) {
+      if (bindingResult.hasErrors()) {
+         model.addAttribute("msg", "모든 입력창을 입력해주세요.");
+         return "member/memberManagement";
+      }
+      if (!memberForm.getMbPw1().equals(memberForm.getMbPw2())) {
+         model.addAttribute("msg", "비밀번호를 확인해주세요.");
+         return "member/memberManagement";
+      }
+      try {
+         this.memberService.create(memberForm.getMbId(), memberForm.getMbPw1(), memberForm.getMbName(),
+               memberForm.getMbCompany());
+      } catch (DuplicateIdException e) {
+         model.addAttribute("msg", "이미 등록된 아이디입니다.");
+         return "member/memberManagement";
+      }
+      return "member/memberManagement";
+   }
 
-	// 관리자 회원가입 시큐리티
-	@PostMapping("/")
-	public String signupRequest(@Valid MemberForm memberForm, BindingResult bindingResult, Model model) {
+   // 유저 요청 회원가입 시큐리티
+   @PostMapping("/")
+   public String signupRequest(@Valid MemberForm memberForm, BindingResult bindingResult, Model model) {
 
-		if (bindingResult.hasErrors()) {
-			model.addAttribute("msg", "예기치 못한 에러가 발생했습니다.");
-			return "member/login_form";
-		}
+      if (bindingResult.hasErrors()) {
+         model.addAttribute("msg", "예기치 못한 에러가 발생했습니다.");
+         return "member/login_form";
+      }
 
-		// 비밀번호 일치 확인
-		if (!memberForm.getMbPw1().equals(memberForm.getMbPw2())) {
-			model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
-			return "member/login_form";
-		}
+      // 비밀번호 일치 확인
+      if (!memberForm.getMbPw1().equals(memberForm.getMbPw2())) {
+         model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
+         return "member/login_form";
+      }
 
-		if(memberService.request(memberForm.getMbId()) == true) {
-			model.addAttribute("msg", "요청이 완료되었습니다");
-			return "member/login_form";
-		}else {
-			model.addAttribute("msg", "동일한 아이디가 있습니다");
-			return "member/login_form";
-		}
-		
-	}
+      if (memberService.request(memberForm.getMbId()) == true) {
+         model.addAttribute("msg", "요청이 완료되었습니다");
+         return "member/login_form";
+      } else {
+         model.addAttribute("msg", "동일한 아이디가 있습니다");
+         return "member/login_form";
+      }
 
-	// ------- 관리자 페이지에서 db로 저장을 하는 컨트롤러 및 서비스 로직 구성
+   }
 
-	// 가장 처음 시작하는 로그인 페이지
-	@GetMapping("/")
-	public String login(Model model) {
-		model.addAttribute("memberForm", new MemberForm());
-		return "member/login_form";
-	}
+   // ------- 관리자 페이지에서 db로 저장을 하는 컨트롤러 및 서비스 로직 구성
 
-	// 로그인이 아무 이상없이 성공하면 main으로 이동함
-	@GetMapping("/main")
-	public String goMain() {
+   // 가장 처음 시작하는 로그인 페이지
+   @GetMapping("/")
+   public String login(Model model) {
+      model.addAttribute("memberForm", new MemberForm());
+      return "member/login_form";
+   }
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+   // 로그인이 아무 이상없이 성공하면 main으로 이동함
+   @GetMapping("/main")
+   public String goMain() {
 
-		if (authentication != null && authentication.isAuthenticated()) {
-			String username = authentication.getName();
-			System.out.println("인증된 사용자: " + username);
-			return "main";
-		} else {
-			System.out.println("사용자가 인증되지 않았습니다");
-			return "member/login_form";
-		}
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-	}
+      if (authentication != null && authentication.isAuthenticated()) {
+         String username = authentication.getName();
+         System.out.println("인증된 사용자: " + username);
+         return "main";
+      } else {
+         System.out.println("사용자가 인증되지 않았습니다");
+         return "member/login_form";
+      }
+
+   }
 
 }
--- CRUD_project/src/main/java/com/crud/service/MemberService.java
+++ CRUD_project/src/main/java/com/crud/service/MemberService.java
@@ -9,40 +9,40 @@ import com.crud.DuplicateIdException;
 import com.crud.entity.Member;
 import com.crud.repository.MemberRepository;
 
-import groovyjarjarpicocli.CommandLine.DuplicateNameException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
 
-	private final MemberRepository memberRepository;
-	private final PasswordEncoder passwordEncoder;
+   private final MemberRepository memberRepository;
+   private final PasswordEncoder passwordEncoder;
 
-	// 유저 회원요청 로직
-	public boolean request(String mbId) {
-		if (memberRepository.findBymbId(mbId).isPresent()) {
-			return false;
-		}else {
-			return true;
-		}
-	}
-	
-	
-	// 관리자 회원가입 로직
-	public Member create(String mbId, String mbPw, String mbName, String mbCompany) {
+   // 유저 회원요청 로직
+   public boolean request(String mbId) {
+      if (memberRepository.findBymbId(mbId).isPresent()) {
+         return false;
+      }else {
+         return true;
+      }
+   }
+   
+   
+   // 관리자 회원가입 로직
+   public Member create(String mbId, String mbPw, String mbName, String mbCompany) {
 
-		if (memberRepository.findBymbId(mbId).isPresent()) {
-			throw new DuplicateIdException("이미 등록된 유저입니다.");
-		}
-		Member member = new Member();
-		member.setMbId(mbId);
-		member.setMbPw(passwordEncoder.encode(mbPw));
-		member.setMbName(mbName);
-		member.setMbCompany(mbCompany);
-		member.setJoineDate(LocalDateTime.now());
-		member.setProfileImg("몰라");
-		this.memberRepository.save(member);
-		return member;
-	}
+      if (memberRepository.findBymbId(mbId).isPresent()) {
+         throw new DuplicateIdException("이미 등록된 유저입니다.");
+      }
+      Member member = new Member();
+      member.setMbId(mbId);
+      member.setMbPw(passwordEncoder.encode(mbPw));
+      member.setMbName(mbName);
+      member.setMbCompany(mbCompany);
+      member.setJoineDate(LocalDateTime.now());
+      member.setProfileImg("local");
+      this.memberRepository.save(member);
+      
+      return member;
+   }
 }
