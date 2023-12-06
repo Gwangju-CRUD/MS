34f77bffbc78f363aea6b4aa17dd6108284dc90e
한창헌 <78711551+Siddhartha110@users.noreply.github.com>
2023-12-06 09:21:32 +0900

    Merge pull request #49 from Gwangju-CRUD/develop
    develop안정화 후 main에 merge

--- CRUD_project/src/main/java/com/crud/service/MemberService.java
+++ CRUD_project/src/main/java/com/crud/service/MemberService.java
@@ -2,13 +2,20 @@ package com.crud.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.crud.DuplicateIdException;
 import com.crud.entity.Member;
+import com.crud.entity.RequestMember;
 import com.crud.repository.MemberRepository;
+import com.crud.repository.RequestMemberRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -17,16 +24,17 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final RequestMemberRepository requestMemberRepository;
+	// 2. 회원가입 요청 메소드 만들기
 
 	// 유저 회원요청 로직
 	public boolean request(String mbId) {
 		if (memberRepository.findBymbId(mbId).isPresent()) {
 			return false;
-		}else {
+		} else {
 			return true;
 		}
 	}
-
 
 	// 관리자 회원가입 로직
 	public Member create(String mbId, String mbPw, String mbName, String mbCompany) {
