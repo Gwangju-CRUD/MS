607e3757499af0e807109a36cbff9f0af75027cd
진영준 <jinange9@gmail.com>
2023-12-06 16:25:39 +0900

    feature/#22 front변경

--- CRUD_project/src/test/java/com/crud/CrudProjectApplicationTests.java
+++ CRUD_project/src/test/java/com/crud/CrudProjectApplicationTests.java
@@ -26,7 +26,7 @@ class CrudProjectApplicationTests {
 	@Test
 	public void db데이터_테스트() {
 		
-		for(int i=1; i<10; i++) {
+		for(int i=1; i<3; i++) {
 			LocalDateTime now = LocalDateTime.now();
 			DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy년 M월 d일 H시 m분");
 			String formatDate = now.format(format);
@@ -58,24 +58,4 @@ class CrudProjectApplicationTests {
 
 		
 	}
-
-	@Test
-	public void 멤버더미데이터(){
-
-		LocalDateTime now = LocalDateTime.now();
-
-		for(int i=51; i<100; i++){
-			Member member = new Member();
-			member.setMbId(String.format("가즈아%d", i));
-			member.setMbName("가즈아");
-			member.setJoineDate(now);
-			member.setMbCompany("가즈아");
-			member.setMbPw("몰라");
-
-			memberRepository.save(member);
-		}
-
-		
-	}
-
 }
