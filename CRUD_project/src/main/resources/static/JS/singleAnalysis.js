81e4a5ab93e495937d3b0afe30b03e293c4737ad
서희주 <139313939+dozzida@users.noreply.github.com>
2023-12-09 11:56:50 +0900

    [FNT] 단건분석 숫자 JS 출력하기 #68

--- CRUD_project/src/main/resources/static/JS/singleAnalysis.js
+++ CRUD_project/src/main/resources/static/JS/singleAnalysis.js
@@ -156,6 +156,11 @@ async function readFilesAndSend(files) {
             $(".h5.mb-0.mr-3.font-weight-bold.text-gray-800").text(
               percentage.toFixed(2) + "%"
             );
+            if (percentage === 100) {
+              setTimeout(() => {
+                location.reload();
+              }, 3000);
+            }
             resolve();
           })
           .catch((error) => {
@@ -370,9 +375,8 @@ window.onload = () => {
   // 대기 시간 조정
   const delayTime = 2000; // 2초후 시작
 
-  // Thymeleaf 변수 사용
   setTimeout(() => {
-    counter($goodCounter, memberCount);  
-    counter($badCounter, singleAnalysisCount);
+    counter($goodCounter, normalCount);  
+    counter($badCounter, errorCount);
 }, delayTime);
 };
